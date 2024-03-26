e6e88b818d61d0fd5a7cf86b02230029f5a920d1
tommywa-a <tomiwa95@gmail.com>
2024-03-26 17:35:24 +0100

    Dashboard setup (admin)

---
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -0,0 +1,37 @@
+import prismadb from "@/lib/prismadb"
+import { auth } from "@clerk/nextjs"
+import { redirect } from "next/navigation"
+
+interface DashboardLayoutProps {
+  children: React.ReactNode
+  params: {
+    storeId: string
+  }
+}
+
+export default async function DashboardLayout({children, params}: {children: React.ReactNode, params:{storeId:string}
+}) {
+  const {userId} = await auth()
+
+  if (!userId) {
+    redirect('/sign-in')
+  }
+
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+      userId
+    }
+  })
+
+  if (!store) {
+    redirect('/')
+  }
+
+  return (
+    <>
+      <div>This will be a navbar</div>
+      {children}
+    </>
+  )
+}
