02b8d6124370846d8b411c62e8e1cee4e29a7de7
tommywa-a <tomiwa95@gmail.com>
2024-03-30 12:52:03 +0100

    modified deprecated next.config.mjs format for images

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["res.cloudinary.com"],
-  }
+export const images = {
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'res.cloudinary.com',
+      port: '',
+      pathname: '/**',
+    },
+  ],
 };
-
-export default nextConfig;
