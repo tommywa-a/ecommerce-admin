d08ccd9aa3c61d8a175a6f18b4c5ee66d5f2f92a
tommywa-a <tomiwa95@gmail.com>
2024-03-30 20:20:32 +0100

    Added filter funcgtionality for billboards

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import React from "react"
+import { BillboardColumn } from "./columns"
+
+interface CellActionProps {
+  data: BillboardColumn
+}
+
+export const CellAction: React.FC<CellActionProps> = ({
+  data
+}) => {
+  return (
+    <div>cellAction</div>
+  )
+}
--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -7,7 +7,14 @@ import { Button } from '@/components/ui/button'
 import { Heading } from '@/components/ui/heading'
 import { Separator } from '@/components/ui/separator'
 
-export const BillboardClient = () => {
+import { BillboardColumn, columns } from './columns'
+import { DataTable } from '@/components/ui/data-table'
+
+interface BillboardClientProps {
+	data: BillboardColumn[]
+}
+
+export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
 	const router = useRouter()
 	const params = useParams()
 
@@ -15,15 +22,22 @@ export const BillboardClient = () => {
 		<>
 			<div className='flex items-center justify-between'>
 				<Heading
-					title='Billboards (0)'
+					title={`Billboards (${data.length})`}
 					description='Manage your billboards.'
 				/>
-				<Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
+				<Button
+					onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+				>
 					<Plus className=' mr-2 h-4 w-4' />
 					Add New
 				</Button>
 			</div>
 			<Separator />
+			<DataTable
+				searchKey='label'
+				columns={columns}
+				data={data}
+			/>
 		</>
 	)
 }
