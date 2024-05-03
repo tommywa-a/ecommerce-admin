5b4d87957c38fdb073fb694e05693ecd7feb4039
tommywa-a <tomiwa95@gmail.com>
2024-05-03 20:26:55 +0100

    Added loader to project

---
+++ components/Loader.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { PuffLoader } from "react-spinners"
+import { ThemeToggle } from "./theme-toggle"
+import { useTheme } from "next-themes"
+
+const Loader = () => {
+  return (
+    <div
+      className="
+        h-[70vh]
+        flex
+        flex-col
+        justify-center
+        items-center
+      "
+    >
+      <PuffLoader
+        size={100}
+        // color={useTheme().theme === 'light' ? '#36d7b7' : '##36d7b7'}
+        color='#36d7b7'
+      />
+    </div>
+  )
+}
+
+export default Loader
