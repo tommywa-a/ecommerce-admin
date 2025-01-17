17dee4cff8e9f97b9506473f53f8701cf6cfef60
Tomiwa Aderibigbe <tomiwa95@gmail.com>
2025-01-17 15:16:43 +0100

    Modified overflow in global.css and spacing of nav on medium devices

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -55,7 +55,7 @@ export function MainNav({
 		},
 	]
 	return (
-		<nav className={cn('flex items-center space-x-2 lg:space-x-6', className)}>
+		<nav className={cn('flex items-center space-x-2 md:space-x-3 lg:space-x-6', className)}>
 			{routes.map((route) => (
 				<Link
 					key={route.href}
